f9f3b9b18c3c20405cc470fbc0a807fee3cfe565
Wander Torres <wander_torres@live.com>
2022-03-09 11:47:27 -0300

    implaments signup page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { SignIn } from "./pages/SignIn";
+import { SignUp } from "./pages/SignUp";
 
 function App(){
     return (
@@ -9,6 +10,9 @@ function App(){
                 <Switch>
                     <Route exact path="/">
                         <SignIn />
+                    </Route>
+                    <Route path="/signup">
+                        <SignUp />
                     </Route>
                 </Switch>
             </Router>
